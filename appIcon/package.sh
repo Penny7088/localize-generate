3346aa80b28fbe74acdbb33a34085eb3fab6f29d
Penny 彭业 <penny@hellotalk.com>
2022-05-10 18:28:17 +0800

    update script

--- appIcon/package.sh
+++ appIcon/package.sh
@@ -2,4 +2,4 @@
 export LANG=en_us.UTF-8
 export LC_ALL=zh_CN.UTF-8
 
-javapackager -deploy -native dmg -name Localize -BappVersion=1.0 -Bicon=appIcon/AppIcon.icns -srcdir . -srcfiles localize-1.0.0-jar-with-dependencies.jar -appclass org.yzr.poi.main.EasyLocalize -outdir . -outfile 123
+javapackager -deploy -native dmg -name Localize -BappVersion=1.0 -Bicon="./AppIcon.icns" -srcdir . -srcfiles localize-1.0.0-jar-with-dependencies.jar -appclass org.yzr.poi.main.EasyLocalize -outdir . -outfile 123
